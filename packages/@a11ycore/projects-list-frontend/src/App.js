462b91408c2dc18a748acb1839e43ad0b8ec5cb7
Evan.Burbidge <burbidge@tcd.ie>
2021-11-10 20:20:22 +0000

    update gitignore

---
+++ packages/@a11ycore/projects-list-frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <p>App.js loaded</p>
+  </div>
+);
+
+export default App;
---
+++ tools/generate.js
@@ -0,0 +1,19 @@
+const { generateTemplateFiles } = require('generate-template-files');
+
+generateTemplateFiles([
+  {
+    option: "create frontend service",
+    defaultCase: "(kebabCase)",
+    entry: {
+      folderPath: "./tools/templates/frontend/"
+    },
+    stringReplacers: ["__PACKAGE__"],
+    output: {
+      path: "./packages/@a11ycore/__PACKAGE__(kebabCase)",
+      pathAndFileNameDefaultCase: "(kebabCase)"
+    },
+    onComplete: results => {
+      console.log(results)
+    }
+  }
+]);
