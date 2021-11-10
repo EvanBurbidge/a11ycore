e91482b704697d8b6aead8b1217f25bdc87694df
Evan.Burbidge <burbidge@tcd.ie>
2021-11-10 20:23:25 +0000

    remove unwanted code

--- services/service1/handler.js
+++ services/service1/handler.js
@@ -1,9 +1,8 @@
 import sample from "sample";
-import lib from "../../libs";
 
 export async function main(event, context) {
   return {
     statusCode: 200,
-    body: `Hello World! Via ${sample()} and ${lib()}.`,
+    body: `Hello World! Via ${sample()}.`,
   };
 }
